8f023932915609b1a0a06b83ea1ebed415d4c527
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-08-26 15:45:59 -0700

    update icon styling

--- src/components/TextCardWithIcon.tsx
+++ src/components/TextCardWithIcon.tsx
@@ -1,5 +1,5 @@
 import { TextCard } from '@/components'
-import { type As, type BoxProps, Grid, Icon, Flex } from '@chakra-ui/react'
+import { type As, type BoxProps, Grid, Icon, Flex, Box } from '@chakra-ui/react'
 
 interface IProps extends BoxProps {
   fadeColor: string
@@ -11,10 +11,37 @@ export const TextCardWithIcon: React.FC<IProps> = ({
   icon,
   fadeColor,
 }) => (
-  <Flex direction={{ base: 'column-reverse', md: 'row' }} gap={12} alignItems="start">
+  <Flex
+    direction={{ base: 'column-reverse', md: 'row' }}
+    gap={12}
+    alignItems="start"
+  >
     <TextCard title={title} fadeColor={fadeColor} flex={4}>
       {children}
     </TextCard>
-    <Icon as={icon} fill={fadeColor} stroke="body" strokeWidth={8} w="full" h="full" maxH={32} flex={1} />
+    <Box flex={1} position="relative" w={32} mx="auto">
+      <Icon
+        as={icon}
+        fill={fadeColor}
+        stroke="body"
+        strokeWidth={0}
+        w="full"
+        h="full"
+        maxH={32}
+        flex={1}
+      />
+      <Icon
+        as={icon}
+        fill="body"
+        w="full"
+        h="full"
+        maxH={32}
+        flex={1}
+        position="absolute"
+        inset={0}
+        zIndex={-1}
+        transform="translate(0.25em, 0.25em)"
+      />
+    </Box>
   </Flex>
 )
